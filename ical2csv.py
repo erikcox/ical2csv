35052fb71b983f55a51fea73ffe3c437d4a9cd18
Erik Cox <erikbcox@gmail.com>
2016-02-04 14:01:46 -0800

    Initial commit

---
+++ ical2csv.py
@@ -0,0 +1,81 @@
+import sys
+import os.path
+from icalendar import Calendar
+import csv
+
+filename = sys.argv[1]
+# TODO: use regex to get file extension (chars after last period), in case it's not exactly 3 chars.
+file_extension = str(sys.argv[1])[-3:]
+headers = ('Summary', 'UID', 'Description', 'Location', 'Start Time', 'End Time', 'URL')
+
+class CalendarEvent:
+    """Calendar event class"""
+    summary = ''
+    uid = ''
+    description = ''
+    location = ''
+    start = ''
+    end = ''
+    url = ''
+
+    def __init__(self, name):
+        self.name = name
+
+event = CalendarEvent("event")
+
+
+def open_cal():
+    if os.path.isfile(filename):
+        if file_extension == 'ics':
+            print "Extracting events from file:", filename, "\n"
+            f = open(sys.argv[1], 'rb')
+            gcal = Calendar.from_ical(f.read())
+
+            # TODO: Let this parse multiple events in one ics file
+            for component in gcal.walk():
+                event.summary = component.get('SUMMARY')
+                event.uid = component.get('UID')
+                event.description = component.get('DESCRIPTION')
+                event.location = component.get('LOCATION')
+                event.start = component.get('dtstart').dt
+                event.end = component.get('dtend').dt
+                event.url = component.get('URL')
+            f.close()
+        else:
+            print "You entered ", filename, ". "
+            print file_extension.upper(), " is not a valid file format. Looking for an ICS file."
+            exit(0)
+    else:
+        print "I can't find the file ", filename, "."
+        print "Please enter an ics file located in the same folder as this script."
+        exit(0)
+
+
+# TODO: make a create and an append method for use with multiple events in one file
+def csv_write(icsfile):
+    csvfile = icsfile[:-3] + "csv"
+    try:
+        with open(csvfile, 'wb') as myfile:
+            values = (event.summary, event.uid, event.description, event.location, event.start, event.end, event.url)
+            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+            wr.writerow(headers)
+            wr.writerow(values)
+            print "Wrote to ", csvfile, "\n"
+    except IOError:
+        print "Could not open file! Please close Excel!"
+        exit(0)
+
+
+def debug_event(class_name):
+    print "Contents of ", class_name.name, ":"
+    print class_name.summary
+    print class_name.uid
+    print class_name.description
+    print class_name.location
+    print class_name.start
+    print class_name.end
+    print class_name.url, "\n"
+
+open_cal()
+csv_write(filename)
+debug_event(event)
